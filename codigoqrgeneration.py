b61394a7a6e5708153789bf3bb9874063f9b70f9
Bastian Landskron <93203724+Blandskron@users.noreply.github.com>
2023-07-29 10:04:05 -0400

    codigoqrgeneration.py

--- codigoqrgeneration.py
+++ codigoqrgeneration.py
@@ -1,20 +1,20 @@
 import qrcode
 
-# Definir la URL del documento
-documento_url = "https://drive.google.com/file/d/1U31wT7suVgbBD80GCGpdZ7Aty-CgewJK/view?usp=sharing"
+# Define the URL of the document
+document_url = "###"
 
-# Crear el código QR
+# Create the QR code
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-qr.add_data(documento_url)
+qr.add_data(document_url)
 qr.make(fit=True)
 
-# Generar la imagen del código QR
-imagen = qr.make_image(fill_color="black", back_color="white")
+# Generate the QR code image
+image = qr.make_image(fill_color="black", back_color="white")
 
-# Guardar la imagen del código QR como archivo
-imagen.save("codigo_qr.png")
+# Save the QR code image as a file
+image.save("qr_code.png")
